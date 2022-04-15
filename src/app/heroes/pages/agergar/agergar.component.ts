2b1393573b4c64776a4a6d01a950bbf7c8d16d3d
aratar79 <aratar79@gmail.com>
2022-04-15 19:33:52 +0200

    First commit

---
+++ src/app/heroes/pages/agergar/agergar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agergar',
+  templateUrl: './agergar.component.html',
+  styleUrls: ['./agergar.component.scss']
+})
+export class AgergarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
