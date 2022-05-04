14995669e8b10f99df149716b730299fb528601d
aratar79 <aratar79@gmail.com>
2022-05-04 07:09:33 +0200

    end the hero finder

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -13,7 +13,7 @@ export class BuscarComponent implements OnInit {
 
   termino: string = '';
   heroes: Heroe[] = [];
-  heroeSeleccionado!: Heroe;
+  heroeSeleccionado: Heroe | undefined;
 
   constructor(private heroesService: HeroesService) { }
 
@@ -21,11 +21,16 @@ export class BuscarComponent implements OnInit {
   }
 
   buscando() {
-    this.heroesService.getHeroesSuggestions(this.termino)
+    this.heroesService.getHeroesSuggestions(this.termino.trim())
       .subscribe(heroes => this.heroes = heroes);
   }
+
   opcionSelecionada(event: MatAutocompleteSelectedEvent) {
 
+    if(!event.option.value) {
+      this.heroeSeleccionado = undefined;
+      return
+    }
     const heroe: Heroe = event.option.value;
     this.termino = heroe.superhero;
 
